8f45a9aa015592ee0f4c25d8e9ceabb62aba28d8
Tony Sepentulevski <tony.sepentulevski@wearesocial.net>
2015-01-05 15:25:40 +1100

    allow dash in db name

--- homestead/scripts/create-mysql.sh
+++ homestead/scripts/create-mysql.sh
@@ -4,7 +4,7 @@ DB=$1;
 mysqlshow -uhomestead -psecret $DB > /dev/null 2>&1
 if [ ! $? -eq 0 ]; then
     echo "Creating database for $DB"
-    mysql -uhomestead -psecret -e "CREATE DATABASE IF NOT EXISTS $DB" > /dev/null 2>&1
+    mysql -uhomestead -psecret -e "CREATE DATABASE IF NOT EXISTS \`$DB\`" > /dev/null 2>&1
 else
     echo "Database for $DB already exists"
 fi
